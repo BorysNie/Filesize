543bc5ea309730a1a279edc645ab9845c995522a
Borys <32977722+BorysNie@users.noreply.github.com>
2019-06-08 16:31:38 +0100

    Working

--- filesize.js
+++ filesize.js
@@ -1,25 +1,51 @@
 const readline = require('readline');
 const fs       = require('fs');
+const path     = require('path');
+const rl       = readline.createInterface({input: process.stdin, output: process.stdout});
 
-var characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
-var theSize    = 0;
+const RANDOM_CHARS = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
-const rl = readline.createInterface({
-    input:  process.stdin,
-    output: process.stdout });
+function askQuestion() {
+  rl.question('Please enter the file size (MB:) ', (user_input) => {
+    let size = parseFloat(user_input);
 
-rl.question('Please enter the file size (MB:) ', (fileSize) => {
-  var fileName = String(fileSize) + "MB.txt";
+    if (isNaN(size) || size <= 0) {
+      console.log("Please enter a valid value!\n");
+      askQuestion();
+    } else {
+      makeFile(size, function () {
+        console.log('done');
+        process.exit(1);
+      });
+    }
+  });
+}
 
-  if (fileSize < 0) {
-    console.log("Please enter a valid value!\n");
-  } else {
-    while (fileSize <= (theSize * 1048576)) { // bytes in 1MB
-      fs.writeFile("./fileName"), characters, function(err) {
-        if (err) throw err;
-          return console.log("File created!\n");
-        rl.close(); process.stdin.destroy();
-      }
+function generateDataInMB(callback) {
+  let byte_to_mb = 1048576;
+  let chars      = RANDOM_CHARS.split('');
+  let result     = '';
+
+  for (let i = 0; i < byte_to_mb; i++) {
+    let char = chars[Math.floor(Math.random() * chars.length)];
+    result   = result + char;
+
+    if (i === (byte_to_mb - 1)) {
+      callback(result);
     }
   }
-});
+}
+
+function makeFile(size, callback) {
+  let name = (size + 'MB.txt');
+  fs.writeFileSync(path.join(__dirname, name), '', 'utf-8');
+
+  for (let i = 0; i < size; i++) {
+    generateDataInMB(function (data) {
+      fs.appendFileSync(name, data);
+    });
+  }
+  callback();
+}
+
+askQuestion();
