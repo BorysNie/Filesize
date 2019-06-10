15d993c36ce531f6f03d6ab63710a73c51660a88
Borys Niedbala <bniedbala@merkleinc.com>
2019-06-10 11:23:20 +0000

    Single function functions

--- filesize.java
+++ filesize.java
@@ -1,11 +1,8 @@
 import java.io.*;
 import java.util.Scanner;
 
-class filesize {
+class Filesize {
   public static void main(String[] args) throws IOException {
-
-    double file_size = 0;
-    String characters = "abcdefghijklmnopqrstuvwxyz123456";
 
     System.out.println("Enter file size (MB): ");
     Scanner x = new Scanner(System.in);
@@ -13,15 +10,27 @@ class filesize {
 
     String file_name = new String(the_size + "MB.txt");
 
+    makeFile(file_name, the_size);
+  }
+
+  static int makeFile(String file_name, float the_size) throws IOException {
+
+    double file_size = 0;
+    String characters = "abcdefghijklmnopqrstuvwxyz123456";
+
     if (the_size > 0) {
       File f = new File(file_name);
       FileWriter file_write = new FileWriter(f);
+
       while (file_size <= (the_size * 1048576)) { // bytes in 1MB
         file_write.write(characters);
         file_size += characters.length(); }
       file_write.close();
+      System.out.println("Done!\n");
+
     } else {
-        System.out.println("Enter a valid number!\n"); }
+      System.out.println("Enter a valid number!\n"); }
+    return 0;
   }
 }
 
