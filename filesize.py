b26c6ae7d3885847c4770a1bcc8a96eb872fbde4
Borys Niedbala <bniedbala@merkleinc.com>
2019-06-10 11:26:51 +0000

    Change logic

--- filesize.py
+++ filesize.py
@@ -2,7 +2,7 @@ the_size  = float(input('Enter file size (MB): '))
 file_size = 0
 string    = 'abcdefghijklmnopqrstuvwxyz0123456789'
 
-if the_size >= 0:
+if the_size > 0:
     File = open(str(the_size) + 'MB.txt', 'w')
     while file_size < (the_size * 1048576):  # bytes in 1MB
         File.write(string)
