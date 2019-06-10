23835b6388732d4e4bbf8fe0ea1772990ad620ed
Borys Niedbala <bniedbala@merkleinc.com>
2019-06-10 11:07:53 +0000

    Single action functions

--- filesize.scala
+++ filesize.scala
@@ -1,26 +1,30 @@
-import java.io._ 
-import scala.io._ 
+import java.io._
 
 object Filesize {
-  def main(args: Array[String]) {
-    var file_size = 0
-    val characters = "abcdefghijklmnopqrstuvwxyz0123456789"
 
-    println("Enter file size (MB:) ")
-    val x = scala.io.StdIn.readLine()
+  var file_size  = 0
+  val characters = "abcdefghijklmnopqrstuvwxyz0123456789"
 
-    val the_size = x.toFloat
-    val file_name = (the_size + "MB.txt")
-
+  def createFile(the_size: Float, file_name: String) {
     if (the_size > 0) {
       val f = new File(file_name)
       var file_write = new FileWriter(f)
       while (file_size <= (the_size * 1048576)) { // bytes in 1MB
         file_write.write(characters)
         file_size = file_size + characters.length() }
-      println("File " + file_name + " created!")
       file_write.close()
+      println("File " + file_name + " created!")
     } else {
       println("Enter a valid number! \n") }
+  }
+
+  def main(args: Array[String]) {
+    println("Enter file size (MB:) ")
+    val x = scala.io.StdIn.readLine()
+
+    val the_size  = x.toFloat
+    val file_name = (the_size + "MB.txt")
+
+    createFile(the_size, file_name)
   }
 }
